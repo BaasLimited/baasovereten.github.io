f4a2873268c0039dc3d746dee3bf8ee73486117c
Sander Baas <sander@implode.nl>
2020-10-11 19:40:56 +0200

    Open links inside the content in a new window

--- assets/js/baasover.js
+++ assets/js/baasover.js
@@ -1,6 +1,29 @@
+/**
+ * Deobfuscate email on click
+ */
 const e = document.getElementById('my-email');
-e.onclick = function(ev) {
-    const e = document.getElementById('my-email');
-    const obf = e.getAttribute('href');
-    e.setAttribute('href', obf.replaceAll(/\.idj.{3}eidk\./gi,''));
+if (e) {
+    e.onclick = function (ev) {
+        const e = document.getElementById('my-email');
+        const obf = e.getAttribute('href');
+        e.setAttribute('href', obf.replaceAll(/\.idj.{3}eidk\./gi, ''));
+    }
+}
+
+/**
+ * Open links in .my-content paragraphs in a new window
+ */
+const mcs = document.getElementsByClassName('my-content');
+for (let i = 0; i < mcs.length; i++) {
+    let pp = mcs[i].getElementsByTagName('p');
+    for (let j = 0; j < pp.length; j++) {
+        let aa = pp[j].getElementsByTagName('a');
+        for (let k = 0; k < aa.length; k++) {
+            let a = aa[k];
+            a.onclick = function (ev) {
+                window.open(this.href);
+                return false;
+            };
+        }
+    }
 }
